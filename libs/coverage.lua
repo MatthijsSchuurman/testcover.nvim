763151d7a759d4ff770d0a7d1060bfdf21d57e42
Matthijs Schuurman <git@matthijsschuurman.nl>
2024-11-10 15:07:10 +0100

    added basic coverage parsing

--- libs/coverage.lua
+++ libs/coverage.lua
@@ -1,7 +1,80 @@
 local Coverage = {}
 
-function Coverage.parse(file)
-  return {}
+function Coverage.parse(filename)
+  local r, error
+  local type = string.match(filename, ".*[/\\](.*)")
+
+  if type == "coverage.out" then
+    r, error = Coverage.parse_gcov(filename)
+  else
+    return nil, {
+      type = "error",
+      section = "Coverage.parse",
+      message = "Unsupported coverage file type",
+      data = {
+        filename = filename,
+        type = type,
+      }
+    }
+  end
+
+
+  return r, error
+end
+
+function Coverage.parse_gcov(filename)
+  local r = {}
+
+  local file = io.open(filename, "r")
+  if not file then
+    return nil, {
+      type = "error",
+      section = "Coverage.parse_gcov",
+      message = "Could not open file",
+      data = {
+        filename = filename,
+      }
+    }
+  end
+
+  for line in file:lines() do
+    if string.match(line, "^mode:") then
+      if not string.match(line, "set") then
+        return nil, {
+          type = "error",
+          section = "Coverage.parse_gcov",
+          message = "Only mode: set is supported",
+          data = {
+            filename = filename,
+            line = line,
+          }
+        }
+      end
+    else
+      local coveredFilename, startLine, startColumn, endLine, endColumn, covered, count = string.match(line, "(.*):(%d+)%.(%d+),(%d+)%.(%d+) (%d+) (%d+)")
+      if coveredFilename then
+        if not r[coveredFilename] then
+          r[coveredFilename] = {}
+        end
+
+        table.insert(r[coveredFilename], {
+          start = {
+            line = tonumber(startLine),
+            column = tonumber(startColumn),
+          },
+          finish = {
+            line = tonumber(endLine),
+            column = tonumber(endColumn),
+          },
+          covered = (tonumber(covered) > 0),
+          count = tonumber(count),
+        })
+      end
+    end
+  end
+
+  file:close()
+  return r
 end
 
 return Coverage
--- libs/tests/coverage_test.lua
+++ libs/tests/coverage_test.lua
@@ -1,2 +1,72 @@
+local coverage = require("testcover.libs.coverage")
+
 describe("Coverage", function()
+  it("should parse coverage file", function()
+    -- Given
+    local coverageFile = "libs/tests/data/coverage.out"
+    local expectedCoverage = {
+      ["example/example.go"] = {
+        {
+          start = {
+            line = 3,
+            column = 21
+          },
+          finish = {
+            line = 5,
+            column = 2
+          },
+          covered = true,
+          count = 1
+        },
+      }
+    }
+
+    -- When
+    local coverageResults, error = coverage.parse(coverageFile)
+
+    -- Then
+    assert.is_nil(error)
+    assert.same(expectedCoverage, coverageResults)
+  end)
+
+  it("should error on file issue", function()
+    -- Given
+    local coverageFile = "doesntexist/coverage.out"
+
+    -- When
+    local coverageResults, error = coverage.parse(coverageFile)
+
+    -- Then
+    assert.is_nil(coverageResults)
+    assert.is_not_nil(error)
+    assert.same({
+      type = "error",
+      section = "Coverage.parse_gcov",
+      message = "Could not open file",
+      data = {
+        filename = coverageFile
+      }
+    }, error)
+  end)
+
+  it("should error on unsupported file", function()
+    -- Given
+    local coverageFile = "libs/tests/data/unsupported.out"
+
+    -- When
+    local coverageResults, error = coverage.parse(coverageFile)
+
+    -- Then
+    assert.is_nil(coverageResults)
+    assert.is_not_nil(error)
+    assert.same({
+      type = "error",
+      section = "Coverage.parse",
+      message = "Unsupported coverage file type",
+      data = {
+        filename = coverageFile,
+        type = "unsupported.out"
+      }
+    }, error)
+  end)
 end)
